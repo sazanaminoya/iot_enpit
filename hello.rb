840139f95e8bd04e2a507df9f456d11b82c6cbd0
vagrant <vagrant@enpit>
2015-09-26 07:09:39 +0000

    Add dynamic change page

--- hello.rb
+++ hello.rb
@@ -11,6 +11,14 @@ get '/alert' do
   erb :views2 
 end
 
+get '/alert2' do
+  erb :views3 
+end
+
+get '/alert3' do
+  erb :views4 
+end
+
 #URL /hoge でアクセス
 get '/hoge' do
   @content = "main content"
